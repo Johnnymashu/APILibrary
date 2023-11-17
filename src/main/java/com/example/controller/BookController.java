eac4214b6f5b1b771c26b7a79bc84d5abd928328
Johnnymashu <Johnnymashu@gmail.com>
2023-11-17 18:33:40 +0000

    Update BookController.java

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -19,24 +20,24 @@ public class BookController {
     public BookController(BookService bookService){
         this.bookService = bookService;
     }
-@GetMapping("/books")
-    public List<Book> findAll(){
-       List<Book> books = bookService.findAll();
-        return books;
-}
+//@GetMapping("/books")
+//    public List<Book> findAll(){
+//       List<Book> books = bookService.findAll();
+//        return books;
+//}
 
-//    @GetMapping("/books")
-//    public List<Book> findAll(@PathParam("filter")String filter){
-//        List<Book> books = Collections.emptyList();
-//        if(StringUtils.isNotBlank("filter")){
-//            books = bookService.findByTitleContains(filter);
-//        }
-//        else {
-//            books = bookService.findAll();
-//        }
-//            return books;
-//        }
-//
+    @GetMapping("/books")
+    public List<Book> findAll(@PathParam("filter")String filter){
+        List<Book> books = Collections.emptyList();
+        if(StringUtils.isNotBlank(filter)){
+            books = bookService.findByTitleContains(filter);
+        }
+        else {
+            books = bookService.findAll();
+        }
+            return books;
+        }
+
         @GetMapping("/books/{id}")
     public Book getBook(@PathVariable int id){
         return bookService.findById(id);
